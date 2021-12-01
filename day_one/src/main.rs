da9378ad753f127d530f21abeea7e8f6d0f0cadc
jack-cooper <cooper.jackwilliam@gmail.com>
2021-12-01 19:51:55 +0000

    feat(day_one): Solved part 1

--- day_one/src/main.rs
+++ day_one/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use std::{
+    fs::File,
+    io::{self, BufRead, BufReader},
+};
+
+fn main() -> io::Result<()> {
+    let input = File::open("day_one/assets/input.txt")?;
+
+    let depths: Vec<u32> = BufReader::new(input)
+        .lines()
+        .flat_map(|line| line.map(|value| value.parse::<u32>()))
+        .flatten()
+        .collect();
+
+    let depths_greater_than_previous =
+        depths
+            .windows(2)
+            .fold(0, |depths_greater_than_previous, depths| {
+                if depths[1] > depths[0] {
+                    depths_greater_than_previous + 1
+                } else {
+                    depths_greater_than_previous
+                }
+            });
+
+    println!(
+        "Depths greater than previous: {}",
+        depths_greater_than_previous
+    );
+
+    Ok(())
 }
