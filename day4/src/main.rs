c7e4a61ea503d76bdd92619d637a105341a90c46
jack-cooper <cooper.jackwilliam@gmail.com>
2021-12-04 20:29:25 +0000

    feat(day4): Added solution to lose to giant squid

--- day4/src/main.rs
+++ day4/src/main.rs
@@ -26,15 +26,18 @@ fn main() -> io::Result<()> {
         })
         .collect();
 
-    if let Some(score) = get_winning_board_score(numbers_draw, boards) {
+    if let Some(score) = get_winning_board_score(&numbers_draw, &boards) {
         println!("Winning board score: {}", score);
+    }
+    if let Some(score) = get_losing_board_score(&numbers_draw, boards) {
+        println!("Losing board score: {}", score);
     }
 
     Ok(())
 }
 
 // Part one
-fn get_winning_board_score(numbers_draw: Vec<u8>, boards: Vec<Board>) -> Option<u16> {
+fn get_winning_board_score(numbers_draw: &[u8], boards: &[Board]) -> Option<u16> {
     let mut selected_numbers: Vec<u8> = Vec::new();
 
     for number in numbers_draw.iter() {
@@ -44,6 +47,29 @@ fn get_winning_board_score(numbers_draw: Vec<u8>, boards: Vec<Board>) -> Option<
             if board.is_won(&selected_numbers) {
                 return Some(board.score(&selected_numbers));
             }
+        }
+    }
+
+    None
+}
+
+// Part two
+fn get_losing_board_score(numbers_draw: &[u8], mut boards: Vec<Board>) -> Option<u16> {
+    let mut selected_numbers: Vec<u8> = Vec::new();
+
+    let mut winning_board: Option<Board> = None;
+
+    for number in numbers_draw.iter() {
+        selected_numbers.push(*number);
+
+        boards.retain(|board| !board.is_won(&selected_numbers));
+
+        if boards.len() == 1 {
+            winning_board = boards.last().map(|board| board.to_owned());
+        }
+
+        if boards.is_empty() {
+            return winning_board.map(|board| board.score(&selected_numbers));
         }
     }
 
