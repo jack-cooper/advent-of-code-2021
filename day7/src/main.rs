e7d8a5b4de088dfc305611e01995d15cdd80ee2b
jack-cooper <cooper.jackwilliam@gmail.com>
2021-12-07 17:52:29 +0000

    feat(day7): Added solution for day 7 part one

--- day7/src/main.rs
+++ day7/src/main.rs
@@ -1,3 +1,31 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> std::io::Result<()> {
+    let input = std::fs::read_to_string("assets/input.txt")?;
+
+    let positions: Vec<i32> = input.trim().split(',').flat_map(str::parse).collect();
+
+    let (minimum_position, maximum_position) =
+        positions
+            .iter()
+            .fold((i32::MAX, i32::MIN), |min_max_positions, position| {
+                (
+                    (*position).min(min_max_positions.0),
+                    (*position).max(min_max_positions.1),
+                )
+            });
+
+    if let Some(minimum_distance) = (minimum_position..=maximum_position)
+        .map(|position| calculate_cumulative_distance_to_target(position, &positions))
+        .min()
+    {
+        dbg!(minimum_distance);
+    }
+
+    Ok(())
+}
+
+fn calculate_cumulative_distance_to_target(target: i32, positions: &[i32]) -> i32 {
+    positions
+        .iter()
+        .map(|position| (position - target).abs())
+        .sum()
 }
