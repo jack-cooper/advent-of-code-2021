ebb50b8bea6ae93726f20a4362e355d41be82fdf
jack-cooper <jack.cooper@stratumfive.com>
2021-12-08 18:49:41 +0000

    feat(day8): Added answer to part one

--- day8/src/main.rs
+++ day8/src/main.rs
@@ -1,5 +1,28 @@
-fn main() {
-    let input = std::fs::read_to_string("assets/input.txt");
+fn main() -> std::io::Result<()> {
+    let input = std::fs::read_to_string("assets/input.txt")?;
 
-    dbg!(input);
+    dbg!(calculate_output_1478_digits(&input));
+
+    Ok(())
+}
+
+fn calculate_output_1478_digits(input: &str) -> usize {
+    input
+        .lines()
+        .flat_map(|line| {
+            let mut entry = line.split(" | ");
+
+            entry.next();
+            entry.next()
+        })
+        .flat_map(|output_values| output_values.split(' '))
+        .filter(|output_value| {
+            let number_of_signals = output_value.len();
+
+            number_of_signals == 2
+                || number_of_signals == 3
+                || number_of_signals == 4
+                || number_of_signals == 7
+        })
+        .count()
 }
