9a3c0f5d0cd0f1f3df97e72c27dc5b5aba1be36c
jack-cooper <jack.cooper@stratumfive.com>
2021-12-11 09:43:28 +0000

    feat(day9): Added solution for part 2
    I never really took the time to learn things like BFS/DFS/Flood fill so doing this without looking any help up online was F-U-N fun

--- day9/src/main.rs
+++ day9/src/main.rs
@@ -1,3 +1,5 @@
+use std::collections::HashMap;
+
 const LINE_LENGTH: usize = 100;
 
 fn main() -> std::io::Result<()> {
@@ -11,44 +13,105 @@ fn main() -> std::io::Result<()> {
 
     let low_points = get_low_points(&heightmap);
 
-    let total_risk_level = low_points
+    let total_risk_level = low_points.iter().fold(0, |total_risk_level, (_, height)| {
+        total_risk_level + height + 1
+    });
+
+    let basin_sizes = get_basin_sizes(&low_points, &heightmap);
+
+    let largest_basins = basin_sizes
         .iter()
-        .fold(0, |total_risk_level, height| total_risk_level + height + 1);
+        .fold([0, 0, 0], |mut largest_basins, &basin| {
+            if basin > largest_basins[0] {
+                largest_basins[2] = largest_basins[1];
+                largest_basins[1] = largest_basins[0];
+                largest_basins[0] = basin;
+            } else if basin > largest_basins[1] {
+                largest_basins[2] = largest_basins[1];
+                largest_basins[1] = basin;
+            } else if basin > largest_basins[2] {
+                largest_basins[2] = basin;
+            }
+
+            largest_basins
+        });
+
+    let largest_basin_size_product = largest_basins
+        .iter()
+        .fold(1, |size_product, &basin_size| size_product * basin_size);
 
     dbg!(total_risk_level);
+    dbg!(largest_basin_size_product);
 
     Ok(())
 }
 
-fn get_adjacent_heights(index: usize, heightmap: &[u32]) -> Vec<u32> {
-    let mut adjacent_indices = Vec::new();
+fn get_adjacent_locations(index: usize, heightmap: &[u32]) -> HashMap<usize, u32> {
+    let mut adjacent_locations: HashMap<usize, u32> = HashMap::new();
 
     if index % LINE_LENGTH != 99 {
-        adjacent_indices.push(heightmap[index + 1]);
+        let adjacent_index = index + 1;
+        adjacent_locations.insert(adjacent_index, heightmap[adjacent_index]);
     }
     if index % LINE_LENGTH != 0 {
-        adjacent_indices.push(heightmap[index - 1]);
+        let adjacent_index = index - 1;
+        adjacent_locations.insert(adjacent_index, heightmap[adjacent_index]);
     }
     if index > 99 {
-        adjacent_indices.push(heightmap[index - 100]);
+        let adjacent_index = index - 100;
+        adjacent_locations.insert(adjacent_index, heightmap[adjacent_index]);
     }
     if index < heightmap.len() - 100 {
-        adjacent_indices.push(heightmap[index + 100]);
+        let adjacent_index = index + 100;
+
+        adjacent_locations.insert(adjacent_index, heightmap[adjacent_index]);
     }
 
-    adjacent_indices
+    adjacent_locations
+}
+
+// Part two
+fn get_basin_sizes(low_points: &HashMap<usize, u32>, heightmap: &[u32]) -> Vec<usize> {
+    low_points
+        .iter()
+        .map(|low_point| get_basin_size(low_point, heightmap))
+        .collect()
 }
 
 // Part one
-fn get_low_points(heightmap: &[u32]) -> Vec<u32> {
+fn get_low_points(heightmap: &[u32]) -> HashMap<usize, u32> {
     heightmap
         .iter()
         .enumerate()
         .filter(|&(index, height)| {
-            get_adjacent_heights(index, &heightmap)
+            get_adjacent_locations(index, &heightmap)
                 .iter()
-                .all(|adjacent_height| adjacent_height > height)
+                .all(|(_, adjacent_height)| adjacent_height > height)
         })
-        .map(|(_, &height)| height)
+        .map(|(index, &height)| (index, height))
         .collect()
+}
+
+fn get_basin_size(low_point: (&usize, &u32), heightmap: &[u32]) -> usize {
+    let mut indices_visited = vec![*low_point.0];
+
+    let mut current_location = (*low_point.0, *low_point.1);
+
+    let mut locations_to_visit: HashMap<usize, u32> =
+        get_adjacent_locations(current_location.0, heightmap);
+
+    while locations_to_visit.len() > 0 {
+        locations_to_visit
+            .retain(|index, &mut height| !indices_visited.contains(index) && height != 9);
+
+        if let Some(new_location) = locations_to_visit.iter().nth(0) {
+            current_location = (*new_location.0, *new_location.1);
+
+            indices_visited.push(current_location.0);
+            locations_to_visit.remove(&current_location.0);
+            locations_to_visit.extend(get_adjacent_locations(current_location.0, heightmap));
+        }
+    }
+
+    indices_visited.len()
 }
